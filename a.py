31c6cf3f2f5e914effb1f652206f35568d0e4d34
nadoyyelek <sigamingliar99@gmail.com>
2025-01-26 21:52:52 +0700

    Update a.py

--- a.py
+++ a.py
@@ -6,7 +6,7 @@ url = "http://pina.infy.uk/"  # Ganti dengan URL target Anda
 
 # Ambil cookie JWT dari respons server
 cookies = {
-    "jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJyb2xlIjoiZ3Vlc3QxMjMiLCJpYXQiOjE3Mzc4OTkxMjEsImV4cCI6MTczNzkwMjcyMX0.7Lxw0cLYxrxbAL3pohur2l4Te6gmvWfXpGYCzyyUGLE"  # Ganti dengan nilai JWT Anda
+    "jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJyb2xlIjoiZ3Vlc3QxMjMiLCJpYXQiOjE3Mzc5MDMxMjYsImV4cCI6MTczNzkwNjcyNn0.xyjuoJgo9e5gmVGU93EttjK2x3jSMxSfdCD-xaLfBQQ"  # Ganti dengan nilai JWT Anda
 }
 
 # Fungsi untuk brute-force secret key
